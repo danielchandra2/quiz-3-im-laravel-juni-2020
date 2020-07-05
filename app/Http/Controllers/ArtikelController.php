e07194186cdaf36d38d3150fe518005b6668d3c3
danielchandra2 <56572103+danielchandra2@users.noreply.github.com>
2020-07-05 15:32:08 +0700

    edit sbadmin2 template dll

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ArtikelModel;
 
 class ArtikelController extends Controller
 {
-    //
+    public function index(){
+	return view('artikel.index');
+    }
+
+    public function create(){
+    return view('artikel.form');
+    }
+
+    public function store(){
+    }
+
+    public function show(){
+    return view('artikel.detail');
+    }
+
+    public function edit(){
+    return view('artikel.edit');
+    }
+
+    public function update(){
+    
+    }
+
+    public function destroy(){
+    	
+    }
 }
---
+++ app/Models/ArtikelModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\ArtikelModel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ArtikelModel extends Model
+{
+	//mengambil semua data
+	public static function get_all(){
+
+	}
+
+	//melakukan insert data ke tabel
+	public static funtion save($data){
+
+	}
+
+    
+}
